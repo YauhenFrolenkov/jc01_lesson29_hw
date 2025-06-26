510d7cb06113bf4efc58241a99a7bd10c53f5b9c
YauhenFrolenkov <esfrolenkov@icloud.com>
2025-06-26 19:28:08 +0300

    Final version

---
+++ src/com/edu/banking_system/util/AccountParser.java
@@ -0,0 +1,39 @@
+package com.edu.banking_system.util;
+
+import java.time.LocalDate;
+
+import com.edu.banking_system.model.Account;
+import com.edu.banking_system.model.Client;
+
+public class AccountParser {
+	
+	public static String serialize(Account account) {
+        return String.join(";",
+                account.getId(),
+                account.getOwner().getName(),
+                account.getOwner().getEmail(),
+                account.getCreationDate().toString(),
+                String.valueOf(account.getBalance())
+        );
+    }
+
+    public static Account deserialize(String line) {
+        String[] parts = line.split(";");
+        if (parts.length != 5) {
+            throw new IllegalArgumentException("Invalid account format: " + line);
+        }
+
+        String id = parts[0];
+        String name = parts[1];
+        String email = parts[2];
+        LocalDate date = LocalDate.parse(parts[3]);
+        double balance = Double.parseDouble(parts[4]);
+
+        Client client = new Client(name, email);
+        Account account = new Account(id, client);
+        account.setCreationDate(date);
+        account.setBalance(balance);
+        return account;
+    }
+
+}
